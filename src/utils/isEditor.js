27e857836e3d9a8c7be779765f1bdd204c4de1d8
nickhealeuromoney <71383721+nickhealeuromoney@users.noreply.github.com>
2020-09-21 17:31:32 +0100

    Add files via upload

--- src/utils/isEditor.js
+++ src/utils/isEditor.js
@@ -2,5 +2,6 @@ export default function isEditor(props) {
   if (!props?.location?.href || typeof document === 'undefined' || typeof window === 'undefined') {
     return true;
   }
-  return props.location.href.includes('.stackbit.dev');
+  return !props.location.href.startsWith('http://localhost');
+  // return props.location.href.includes('.stackbit.dev');
 }
